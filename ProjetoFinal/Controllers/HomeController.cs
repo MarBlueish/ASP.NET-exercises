22665f2f16b967c75065b8cd38d0743b62d85964
MarBlueish <6958234@formacao.iefp.pt>
2024-02-29 18:01:07 +0000

    changes

--- ProjetoFinal/Controllers/HomeController.cs
+++ ProjetoFinal/Controllers/HomeController.cs
@@ -2,31 +2,36 @@
 using ProjetoFinal.Models;
 using System.Diagnostics;
 
-namespace ProjetoFinal.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult Casa()
+    {
+        // Lógica da ação Casa aqui
+        return View("Home"); // Alterado para retornar a view "Home"
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
+
+
---
+++ ProjetoFinal/Models/VisitaViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoFinal.Models
+{
+    public class VisitaViewModel
+    {
+        public Visita Visita { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
